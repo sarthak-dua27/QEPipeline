cf66c9a6a8004fe81a27b18b7fa1cf276082094b
SarthakDua <sarthak.dua@couchbase.com>
2023-01-18 15:55:27 +0530

    abcd

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -1,9 +1,14 @@
 @Library('shared-library') _
 pipeline {
   agent any
+  tools{
+    maven 'maven'
+  }
   stages {
     stage('stage 1') {
       steps {
+        mvn --version
+        mvn clean install
         helloWorld()
       }
     }
