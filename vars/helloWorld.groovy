b477dfef7a974e2aec69d9315ab5dc2a75c447ea
SarthakDua <sarthak.dua@couchbase.com>
2023-01-18 17:06:20 +0530

    abcd

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,62 +1,59 @@
-//import static com.couchbase.client.java.kv.MutateInSpec.insert;
-//import java.time.Duration;
-//import com.couchbase.client.java.Bucket;
-//import com.couchbase.client.java.Cluster;
-//import com.couchbase.client.java.Collection;
-//import com.couchbase.client.java.Scope;
-//import com.couchbase.client.java.json.JsonObject;
-//import com.couchbase.client.java.kv.MutationResult;
+import static com.couchbase.client.java.kv.MutateInSpec.insert;
+import java.time.Duration;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.Scope;
+import com.couchbase.client.java.json.JsonObject;
+import com.couchbase.client.java.kv.MutationResult;
 
 
-def call(){
+def call(String username, String password, String connectstring, String name, String actions){
   sh "echo Hello World"
-}
+  Cluster cluster = Cluster.connect("couchbases://" + connectstring, username, password);
+  Bucket bucket = cluster.bucket("qe24_status_sarthak");
+  bucket.waitUntilReady(Duration.ofSeconds(120));
+  Scope scope = bucket.scope("_default");
+  Collection collection = scope.collection("_default");
 
-//def call(Map config = [:]){
-//  sh "echo Hello World"
-//  Cluster cluster = Cluster.connect("couchbases://" + config.connectstring, config.username as String, config.password as String);
-//  Bucket bucket = cluster.bucket("qe24_status");
-//  bucket.waitUntilReady(Duration.ofSeconds(120));
-//  Scope scope = bucket.scope("_default");
-//  Collection collection = scope.collection("_default");
-//
-//  if(config.actions=="checkname"){
-//    try{
-//      result = collection.get(config.name as String)
-//      println("Found")
-//      return true
-//    }catch (Exception ex){
-//      JsonObject env = JsonObject.create().put(config.key as String, "STARTED").put("latest", false);
-//      JsonObject content = JsonObject.create().put("AMI", config.name as String).put(config.env as String, env);
-//      MutationResult insertResult = collection.insert(config.name as String, content);
-//      println("Doc Created")
-//      return False
-//    }
-//  }else if(config.actions=="update"){
-//    try{
-//      collection.mutateIn("hotel_1368", Collections.singletonList(insert(config.key as String, config.value)));
-//      return "Success"
-//    }catch (Exception ex){
-//      return "Failed"
-//    }
-//  }else if(config.actions=="update_latest"){
-//    try{
-//      statement = "select AMI from `qe24_status` where" + config.env + "latest= TRUE and" +  config.env ".PIPELINE_STATUS=SUCCESS"
-//      QueryResult result = cluster.query(statement);
-//
-//      for (JsonObject row : result.rowsAsObject()) {
-//        collection.mutateIn(row.AMI, Collections.singletonList(insert(config.env+".latest", false)));
-//      }
-//      collection.mutateIn(row.AMI, Collections.singletonList(insert(config.env+".latest", true)));
-//      return true
-//    }catch (Exception ex){
-//      print("FALSE")
-//    }
-//    return false
-//
-//  }else if(config.actions=="get_latest_name"){
-//    println("")
-//  }else{
-//    println("invalid action")
-//  }
-//}
+  if(actions=="checkname"){
+    try{
+      sh 'echo in checkname'
+      result = collection.get(name)
+      println("Found")
+      return true
+    }catch (Exception ex){
+      JsonObject env = JsonObject.create().put(key , "STARTED").put("latest", false);
+      JsonObject content = JsonObject.create().put("AMI", name ).put(env as String, env);
+      MutationResult insertResult = collection.insert(name , content);
+      println("Doc Created")
+      return False
+    }
+  }else if(actions=="update"){
+    try{
+      collection.mutateIn("hotel_1368", Collections.singletonList(insert(key , value)));
+      return "Success"
+    }catch (Exception ex){
+      return "Failed"
+    }
+  }else if(actions=="update_latest"){
+    try{
+      statement = "select AMI from `qe24_status` where" + env + "latest= TRUE and" +  env ".PIPELINE_STATUS=SUCCESS"
+      QueryResult result = cluster.query(statement);
+
+      for (JsonObject row : result.rowsAsObject()) {
+        collection.mutateIn(row.AMI, Collections.singletonList(insert(env+".latest", false)));
+      }
+      collection.mutateIn(row.AMI, Collections.singletonList(insert(env+".latest", true)));
+      return true
+    }catch (Exception ex){
+      print("FALSE")
+    }
+    return false
+
+  }else if(actions=="get_latest_name"){
+    println("")
+  }else{
+    println("invalid action")
+  }
+}
--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -9,7 +9,9 @@ pipeline {
       steps {
         sh 'mvn --version'
         sh 'mvn clean install'
-        helloWorld(new LinkedHashMap<>([username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe"]))
+//        helloWorld(new LinkedHashMap<>([username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe"]))
+//        helloWorld(username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe")
+        helloWorld("sarthak","Password@123","cb.wey5oeokxs3xxgrd.cloud.couchbase.com","pipe","checkname")
       }
     }
   }
