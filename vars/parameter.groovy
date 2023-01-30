3bc5915a8a902c47ba1ac987a80d1046a5f1105e
SarthakDua <sarthak.dua@couchbase.com>
2023-01-30 12:13:45 +0530

    abcd

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -1,14 +1,12 @@
 @Library('shared-library') _
 pipeline {
-  agent any
+  agent { label 'centos-slave' }
   tools{
     maven 'maven'
   }
   stages {
     stage('stage 1') {
       steps {
-        sh 'mvn --version'
-        sh 'mvn clean install'
         helloWorld()
 //        helloWorld(new LinkedHashMap<>([username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe"]))
 //        helloWorld(username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe")
