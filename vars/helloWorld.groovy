40aabafdfa269e0d8c1357b846b8d5e53b25ebc0
SarthakDua <sarthak.dua@couchbase.com>
2023-02-01 12:02:20 +0530

    abcd

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -14,19 +14,19 @@ def call(String username, String password, String connectstring, String name, St
   sh "echo Hello World"
   Cluster cluster = Cluster.connect("couchbases://" + connectstring, username, password);
   sh 'echo cluster connected'
-  Bucket bucket = cluster.bucket("qe24_status_sarthak");
-  bucket.waitUntilReady(Duration.ofSeconds(120));
-  Scope scope = bucket.scope("_default");
-  Collection collection = scope.collection("_default");
+//  Bucket bucket = cluster.bucket("qe24_status_sarthak");
+//  bucket.waitUntilReady(Duration.ofSeconds(120));
+//  Scope scope = bucket.scope("_default");
+//  Collection collection = scope.collection("_default");
 
-  if(actions=="checkname"){
+  if(actions=="checkname") {
     sh 'echo in checkname $1'
-    try{
+    try {
       sh 'echo in checkname $2'
-      result = collection.get(name)
-      println("Found")
+//      result = collection.get(name)
+//      println("Found")
       return true
-    }catch (Exception ex){
+    } catch (Exception ex) {
       sh 'echo in catch of checkname'
 //      JsonObject env = JsonObject.create().put(key , "STARTED").put("latest", false);
 //      JsonObject content = JsonObject.create().put("AMI", name ).put(env as String, env);
@@ -34,31 +34,32 @@ def call(String username, String password, String connectstring, String name, St
 //      println("Doc Created")
       return False
     }
-  }else if(actions=="update"){
-    try{
-      collection.mutateIn("hotel_1368", Collections.singletonList(insert(key , value)));
-      return "Success"
-    }catch (Exception ex){
-      return "Failed"
-    }
-  }else if(actions=="update_latest"){
-    try{
-      statement = "select AMI from `qe24_status` where" + env + "latest= TRUE and" +  env + ".PIPELINE_STATUS=SUCCESS"
-      QueryResult result = cluster.query(statement);
-
-      for (JsonObject row : result.rowsAsObject()) {
-        collection.mutateIn(row.AMI, Collections.singletonList(insert(env+".latest", false)));
-      }
-      collection.mutateIn(row.AMI, Collections.singletonList(insert(env+".latest", true)));
-      return true
-    }catch (Exception ex){
-      print("FALSE")
-    }
-    return false
-
-  }else if(actions=="get_latest_name"){
-    println("")
-  }else{
-    println("invalid action")
+//  }else if(actions=="update"){
+//    try{
+//      collection.mutateIn("hotel_1368", Collections.singletonList(insert(key , value)));
+//      return "Success"
+//    }catch (Exception ex){
+//      return "Failed"
+//    }
+//  }else if(actions=="update_latest"){
+//    try{
+//      statement = "select AMI from `qe24_status` where" + env + "latest= TRUE and" +  env + ".PIPELINE_STATUS=SUCCESS"
+//      QueryResult result = cluster.query(statement);
+//
+//      for (JsonObject row : result.rowsAsObject()) {
+//        collection.mutateIn(row.AMI, Collections.singletonList(insert(env+".latest", false)));
+//      }
+//      collection.mutateIn(row.AMI, Collections.singletonList(insert(env+".latest", true)));
+//      return true
+//    }catch (Exception ex){
+//      print("FALSE")
+//    }
+//    return false
+//
+//  }else if(actions=="get_latest_name"){
+//    println("")
+//  }else{
+//    println("invalid action")
+//  }
   }
 }
