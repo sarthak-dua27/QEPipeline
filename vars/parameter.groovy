2dd103c836265dfa8a0bf91175bd8eb3bed8bea6
SarthakDua <sarthak.dua@couchbase.com>
2023-02-01 12:17:59 +0530

    abcd

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -4,12 +4,10 @@ pipeline {
   stages {
     stage('stage 1') {
       steps {
-//        sh 'mvn --version'
-//        sh 'mvn clean install'
 //        helloWorld()
 //        helloWorld(new LinkedHashMap<>([username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe"]))
 //        helloWorld(username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe")
-        helloWorld("sarthak","Password@123","cb.wey5oeokxs3xxgrd.cloud.couchbase.com","pipe","checkname")
+        helloWorld("sarthak2","Password@123","cb.wey5oeokxs3xxgrd.cloud.couchbase.com","pipe","checkname")
       }
     }
   }
