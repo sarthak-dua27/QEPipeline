c35e0d7eec3384b9232bb10935b30ae677d68118
SarthakDua <sarthak.dua@couchbase.com>
2023-02-01 13:38:50 +0530

    abcd

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -9,7 +9,7 @@ pipeline {
 //        helloWorld()
 //        helloWorld(new LinkedHashMap<>([username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe"]))
 //        helloWorld(username:"sarthak", password:"Password@123",connectstring:"cb.wey5oeokxs3xxgrd.cloud.couchbase.com",name:"pipe")
-        helloWorld("sarthak","Password@123","cb.wey5oeokxs3xxgrd.cloud.couchbase.com","pipe","checkname")
+        helloWorld("sarthak","Password@123","cb.wey5oeokxs3xxgrd.cloud.couchbase.com","asad","checkname")
       }
     }
   }
